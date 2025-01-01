7994204bf6bf02bb3093768f13a3481d242ffb1d
craniacshencil <rajnikamrajnikam@gmail.com>
2025-01-01 10:45:49 +0530

    Add rate-limiter
    - limits API requests on all handlers

--- cmd/main.go
+++ cmd/main.go
@@ -7,14 +7,15 @@ import (
 	"os"
 
 	"github.com/craniacshencil/cachecast/internal"
+	"github.com/craniacshencil/cachecast/utils"
 	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
+	"golang.org/x/time/rate"
 )
 
 func init() {
 	if err := godotenv.Load(); err != nil {
-		log.Println("ERR: While opening .env file")
-		log.Println(err)
+		log.Println("ERR: While opening .env file:", err)
 		return
 	}
 }
@@ -30,22 +31,8 @@ func main() {
 	cacheClient := internal.NewCacheClient(client)
 
 	router := http.NewServeMux()
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.ParseFiles("./web/index.html")
-		if err != nil {
-			log.Println("ERR: While creating template for HTML file")
-			log.Println(err)
-			return
-		}
-
-		err = t.Execute(w, nil)
-		if err != nil {
-			log.Println("ERR: While executing template")
-			log.Println(err)
-			return
-		}
-	})
-	router.HandleFunc("POST /", cacheClient.GetWeather)
+	router.Handle("/", rateLimiter(servePage))
+	router.Handle("POST /", rateLimiter(cacheClient.GetWeather))
 
 	server := &http.Server{
 		Addr:    os.Getenv("SERVER_ADDR"),
@@ -54,8 +41,34 @@ func main() {
 
 	err := server.ListenAndServe()
 	if err != nil {
-		log.Println("ERR: While serving")
-		log.Println(err)
+		log.Println("ERR: While serving ", err)
+		return
+	}
+}
+
+func rateLimiter(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	limiter := rate.NewLimiter(2, 4)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			message := "Too many requests"
+			utils.WriteJSON(w, http.StatusTooManyRequests, message)
+			return
+		} else {
+			next(w, r)
+		}
+	})
+}
+
+func servePage(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./web/index.html")
+	if err != nil {
+		log.Println("ERR: While creating template for HTML file:", err)
+		return
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Println("ERR: While executing template:", err)
 		return
 	}
 }
